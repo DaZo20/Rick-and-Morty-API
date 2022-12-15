ca3f7cf76da38c291a5658142399a196200fd829
David Muñoz <david.munyoz97@gmail.com>
2022-12-15 14:19:21 +0100

    feat: Added domain layer for login feature

--- app/src/main/java/da/zo/rickandmortyapi/login/domain/LoginDomainLayerContract.kt
+++ app/src/main/java/da/zo/rickandmortyapi/login/domain/LoginDomainLayerContract.kt
@@ -1,4 +1,19 @@
 package da.zo.rickandmortyapi.login.domain
 
+import da.zo.rickandmortyapi.login.domain.model.Email
+import da.zo.rickandmortyapi.login.domain.model.LoginUser
+import da.zo.rickandmortyapi.login.domain.model.Password
+
 interface LoginDomainLayerContract {
+    interface PresentationLayer {
+        interface UseCase<T>{
+            suspend operator fun invoke(email: Email, password: Password): Result<T>
+        }
+    }
+
+    interface DataLayer {
+        interface LoginRepository {
+            suspend fun loginUser(email: Email, password: Password): Result<LoginUser>
+        }
+    }
 }
