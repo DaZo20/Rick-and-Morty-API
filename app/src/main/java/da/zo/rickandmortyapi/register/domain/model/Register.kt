dcdb585689e8009b4d61551fe7e131589d3c486e
David Muñoz <david.munyoz97@gmail.com>
2023-01-10 12:54:03 +0100

    feat: Added domain layer for register feature

--- app/src/main/java/da/zo/rickandmortyapi/register/domain/model/Register.kt
+++ app/src/main/java/da/zo/rickandmortyapi/register/domain/model/Register.kt
@@ -1,4 +1,10 @@
 package da.zo.rickandmortyapi.register.domain.model
 
-class Register {
-}
+import da.zo.rickandmortyapi.common.utils.Email
+import da.zo.rickandmortyapi.common.utils.Password
+
+data class RegisterUser(
+    val name: String,
+    val email: Email,
+    val password: Password
+)
--- app/src/main/java/da/zo/rickandmortyapi/register/domain/usecase/CreateUserWithEmailAndPassUC.kt
+++ app/src/main/java/da/zo/rickandmortyapi/register/domain/usecase/CreateUserWithEmailAndPassUC.kt
@@ -1,4 +1,36 @@
 package da.zo.rickandmortyapi.register.domain.usecase
 
-class CreateUserWithEmailAndPassUC {
+import android.nfc.FormatException
+import android.widget.EditText
+import android.widget.Toast
+import da.zo.rickandmortyapi.common.utils.Email
+import da.zo.rickandmortyapi.common.utils.Password
+import da.zo.rickandmortyapi.databinding.ActivityRegisterBinding
+import da.zo.rickandmortyapi.register.domain.RegisterDomainLayerContract
+import da.zo.rickandmortyapi.register.domain.model.RegisterUser
+import da.zo.rickandmortyapi.register.presentation.view.RegisterActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CreateUserWithEmailAndPassUC @Inject constructor(
+    private val registerRepository: RegisterDomainLayerContract.DataLayer.RegisterRepository
+) : RegisterDomainLayerContract.PresentationLayer.UseCase<RegisterUser> {
+
+    private var registerBinding: ActivityRegisterBinding? = null
+
+    override suspend fun invoke(email: Email, password: Password): Result<RegisterUser> =
+        withContext(Dispatchers.IO) {
+            if (registerBinding?.emailEt?.text.toString().isNotEmpty() && registerBinding?.userEt?.text.toString().isNotEmpty()) {
+                if (registerBinding?.registerPasswordEt?.text.toString().equals(registerBinding?.registerConfirmPasswordEt?.text.toString())) {
+                    registerRepository.registerUser(email, password)
+                } else {
+                    Result.failure(FormatException("Passwords must match"))
+                }
+            } else {
+                Result.failure(FormatException("There cannot be any empty fields"))
+            }
+        }
+
+
 }
