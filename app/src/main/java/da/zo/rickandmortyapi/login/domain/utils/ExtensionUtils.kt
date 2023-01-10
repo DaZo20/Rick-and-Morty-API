cbc0936f44f5503b6caf171c1dc29ba07ce86a2e
David Muñoz <david.munyoz97@gmail.com>
2023-01-10 13:03:06 +0100

    feat: domain layer of login feature modified

--- app/src/main/java/da/zo/rickandmortyapi/login/domain/utils/ExtensionUtils.kt
+++ app/src/main/java/da/zo/rickandmortyapi/login/domain/utils/ExtensionUtils.kt
@@ -1,9 +1,12 @@
 package da.zo.rickandmortyapi.login.domain.utils
 
+import da.zo.rickandmortyapi.common.utils.Email
+import da.zo.rickandmortyapi.common.utils.Password
 import da.zo.rickandmortyapi.login.data.model.LoginUserDto
-import da.zo.rickandmortyapi.login.domain.model.Email
 import da.zo.rickandmortyapi.login.domain.model.LoginUser
-import da.zo.rickandmortyapi.login.domain.model.Password
+import da.zo.rickandmortyapi.register.data.model.RegisterUserDto
+import da.zo.rickandmortyapi.register.domain.model.RegisterUser
+
 
 private const val DEFAULT_STRING_VALUE = ""
 
@@ -18,4 +21,11 @@ fun LoginUserDto.toLoginUser() =
     LoginUser(
         name = name ?: DEFAULT_STRING_VALUE,
         email = Email(value =  email ?: DEFAULT_STRING_VALUE)
+    )
+
+fun RegisterUserDto.toRegisterUser() =
+    RegisterUser(
+        name = name ?: DEFAULT_STRING_VALUE,
+        email = Email(value =  email ?: DEFAULT_STRING_VALUE),
+        password = Password(value = DEFAULT_STRING_VALUE )
     )
