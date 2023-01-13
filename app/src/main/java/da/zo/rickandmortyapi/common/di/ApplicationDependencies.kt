7a64b47df0cdb546ef763f97c6dd48a9b4045cc3
David Muñoz <david.munyoz97@gmail.com>
2023-01-13 20:52:37 +0100

    feature: Common files modified

--- app/src/main/java/da/zo/rickandmortyapi/common/di/ApplicationDependencies.kt
+++ app/src/main/java/da/zo/rickandmortyapi/common/di/ApplicationDependencies.kt
@@ -1,15 +1,40 @@
 package da.zo.rickandmortyapi.common.di
 
+import android.content.Context
+import androidx.room.Room
 import com.google.firebase.auth.FirebaseAuth
+import da.zo.rickandmortyapi.common.db.ApplicationDatabase
+import da.zo.rickandmortyapi.common.utils.getRetrofitInstance
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object UtilsModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(converterFactory: Converter.Factory): Retrofit =
+        getRetrofitInstance(converterFactory = converterFactory)
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): Converter.Factory = GsonConverterFactory.create()
+
+
+    @Singleton
+    @Provides
+    fun providesRoomDataBaseInstance(@ApplicationContext applicationContext: Context) : ApplicationDatabase = Room.databaseBuilder(
+        applicationContext,
+        ApplicationDatabase::class.java,"rick-and-morty-db"
+    ).build()
 
     @Singleton
     @Provides
--- app/src/main/java/da/zo/rickandmortyapi/common/utils/ExtensionUtils.kt
+++ app/src/main/java/da/zo/rickandmortyapi/common/utils/ExtensionUtils.kt
@@ -1,5 +1,39 @@
 package da.zo.rickandmortyapi.common.utils
 
+import da.zo.rickandmortyapi.characters.data.db.LocationEntity
+import da.zo.rickandmortyapi.characters.data.db.OriginEntity
+import da.zo.rickandmortyapi.characters.data.model.LocationDto
+import da.zo.rickandmortyapi.characters.data.model.OriginDto
+import da.zo.rickandmortyapi.characters.domain.model.Location
+import da.zo.rickandmortyapi.characters.domain.model.Origin
+
 //
 // Created by DaZo20 on 13/01/2023.
 //
+fun OriginDto.toBo() : Origin = Origin(name = name, url = url)
+
+fun LocationDto.toBo() : Location = Location(name = name, url = url)
+
+fun OriginDto.toEntity(): OriginEntity =
+    OriginEntity(
+        name = name,
+        url = url
+    )
+
+fun LocationDto.toEntity(): LocationEntity =
+    LocationEntity(
+        name = name,
+        url = url
+    )
+
+fun OriginEntity.toBo(): Origin =
+    Origin(
+        name = name,
+        url = url
+    )
+
+fun LocationEntity.toBo(): Location =
+    Location(
+        name = name,
+        url = url
+    )
