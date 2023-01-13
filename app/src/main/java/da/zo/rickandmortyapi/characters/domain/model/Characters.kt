49a8fc9c9b152d84bf7a90df229f4c924cdb8b8f
David Muñoz <david.munyoz97@gmail.com>
2023-01-13 17:19:53 +0100

    feat: Added domain layer for character feature

--- app/src/main/java/da/zo/rickandmortyapi/characters/domain/model/Characters.kt
+++ app/src/main/java/da/zo/rickandmortyapi/characters/domain/model/Characters.kt
@@ -3,3 +3,32 @@ package da.zo.rickandmortyapi.characters.domain.model
 //
 // Created by DaZo20 on 11/01/2023.
 //
+
+data class MultipleCharacters(
+    val characters: List<Character>
+)
+data class Characters(
+    val results: List<Character>
+)
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: Origin,
+    val location: Location,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
+data class Origin(
+    val name: String,
+    val url: String
+)
+data class Location(
+    val name: String,
+    val url: String
+)
--- app/src/main/java/da/zo/rickandmortyapi/characters/domain/usecase/GetCharacterNextPageUc.kt
+++ app/src/main/java/da/zo/rickandmortyapi/characters/domain/usecase/GetCharacterNextPageUc.kt
@@ -1,7 +1,14 @@
 package da.zo.rickandmortyapi.characters.domain.usecase
 
+import da.zo.rickandmortyapi.characters.domain.CharacterDomainLayerContract
+import da.zo.rickandmortyapi.characters.domain.model.Characters
+import javax.inject.Inject
+
 //
 // Created by DaZo20 on 13/01/2023.
 //
-class GetCharacterNextPageUc {
+class GetCharacterNextPageUc @Inject constructor(
+    private val characterRepository: CharacterDomainLayerContract.DataLayer.CharacterRepository
+): CharacterDomainLayerContract.PresentationLayer.UseCase<Characters> {
+    override suspend fun invoke(): Result<Characters> = characterRepository.getCharactersNextPage()
 }
