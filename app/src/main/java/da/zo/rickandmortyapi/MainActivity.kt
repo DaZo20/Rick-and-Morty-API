49952c22ae949d55572befdd5d4b6ccb09c2424e
David Muñoz <david.munyoz97@gmail.com>
2023-02-21 14:43:05 +0100

    feat: Added some drawables to the project

--- app/src/main/java/da/zo/rickandmortyapi/MainActivity.kt
+++ app/src/main/java/da/zo/rickandmortyapi/MainActivity.kt
@@ -1,36 +1,43 @@
 package da.zo.rickandmortyapi
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 import da.zo.rickandmortyapi.characters.presentation.view.CharacterFragment
+import da.zo.rickandmortyapi.characters.presentation.viewmodel.CharactersViewModel
 import da.zo.rickandmortyapi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), OnItemSelectedListener {
     lateinit var mainBinding: ActivityMainBinding
-    private val charactersFragment: Fragment by lazy { CharacterFragment.newInstance()}
-   // private val episodesFragment: Fragment by lazy { EpisodesFragment.newInstance() }
+    private val characterViewModel: CharactersViewModel by viewModels()
+    private val charactersFragment: Fragment by lazy { CharacterFragment.newInstance() }
+    // private val episodesFragment: Fragment by lazy { EpisodesFragment.newInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
         initViews(savedState = savedInstanceState)
     }
 
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.navigation_characters -> {
                 replaceWithCharactersFragment()
                 return true
             }
             R.id.navigation_episodes -> {
-                replaceWithEpisodesFragment()
+//                replaceWithEpisodesFragment()
                 return true
             }
         }
@@ -53,8 +60,11 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_view, charactersFragment).commit()
     }
+
     private fun replaceWithEpisodesFragment() {
         //supportFragmentManager.beginTransaction()
-          //  .replace(R.id.fragment_view, episodesFragment).commit()
+        //  .replace(R.id.fragment_view, episodesFragment).commit()
     }
 }
+
+
