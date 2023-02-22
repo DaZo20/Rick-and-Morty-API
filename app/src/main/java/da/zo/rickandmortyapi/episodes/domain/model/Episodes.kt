9dccbb771e92d3dc50772859665eee683cc78fc7
David Muñoz <david.munyoz97@gmail.com>
2023-02-22 21:12:45 +0100

    feat: Added domain layer to Episodes feature

--- app/src/main/java/da/zo/rickandmortyapi/episodes/domain/model/Episodes.kt
+++ app/src/main/java/da/zo/rickandmortyapi/episodes/domain/model/Episodes.kt
@@ -4,5 +4,16 @@ package da.zo.rickandmortyapi.episodes.domain.model
 // Created by DaZo20 on 22/02/2023.
 // Copyright (c) 2023 DZ. All rights reserved.
 //
-class Episodes {
-}
+data class Episodes(
+    val results: List<Episode>
+)
+
+data class Episode(
+    val id: Int,
+    val name: String,
+    val airDate: String,
+    val episode: String,
+    val characters: List<String>,
+    val url: String,
+    val created: String
+)
--- app/src/main/java/da/zo/rickandmortyapi/episodes/domain/usecase/GetAllEpisodesUC.kt
+++ app/src/main/java/da/zo/rickandmortyapi/episodes/domain/usecase/GetAllEpisodesUC.kt
@@ -1,8 +1,17 @@
 package da.zo.rickandmortyapi.episodes.domain.usecase
 
+import da.zo.rickandmortyapi.episodes.domain.EpisodesDomainLayerContract
+import da.zo.rickandmortyapi.episodes.domain.model.Episodes
+import javax.inject.Inject
+
 //
 // Created by DaZo20 on 22/02/2023.
 // Copyright (c) 2023 DZ. All rights reserved.
 //
-class GetAllEpisodesUC {
+class GetAllEpisodesUC @Inject constructor(
+    private val episodeRepository: EpisodesDomainLayerContract.DataLayer.EpisodeRepository
+): EpisodesDomainLayerContract.PresentationLayer.UseCase<Episodes> {
+    override suspend fun invoke(): Result<Episodes> = episodeRepository.getAllEpisodesList()
+
+
 }
