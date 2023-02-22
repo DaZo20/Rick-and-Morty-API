86e91259b56ef4f78dddbdd4bcf1da742bbc14e2
David Muñoz <david.munyoz97@gmail.com>
2023-02-22 21:11:35 +0100

    feat: Added presentation layer to Episodes feature

--- app/src/main/java/da/zo/rickandmortyapi/episodes/presentation/view/EpisodeAdapter.kt
+++ app/src/main/java/da/zo/rickandmortyapi/episodes/presentation/view/EpisodeAdapter.kt
@@ -1,8 +1,50 @@
 package da.zo.rickandmortyapi.episodes.presentation.view
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import da.zo.rickandmortyapi.R
+import da.zo.rickandmortyapi.episodes.domain.model.Episode
+
 //
 // Created by DaZo20 on 22/02/2023.
 // Copyright (c) 2023 DZ. All rights reserved.
 //
-class EpisodeAdapter {
+class EpisodeAdapter (
+    private val episodeData: MutableList<Episode> = mutableListOf()
+        ): RecyclerView.Adapter<EpisodeAdapter.EpisodeViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodeViewHolder {
+        val rootView: View = LayoutInflater.from(parent.context).inflate(R.layout.episodes_adapter_row, parent, false)
+        return EpisodeViewHolder(itemView = rootView)
+    }
+
+    override fun onBindViewHolder(holder: EpisodeViewHolder, position: Int) =
+        holder.bindData(episodeData[position])
+
+
+    override fun getItemCount(): Int = episodeData.size
+
+    fun updateData(newData: List<Episode>) {
+        episodeData.addAll(newData.toMutableList())
+        notifyDataSetChanged()
+    }
+
+    inner class EpisodeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvName: TextView by lazy { itemView.findViewById(R.id.tv_name) }
+        private val tvEpisode: TextView by lazy { itemView.findViewById(R.id.tv_episode) }
+        private val tvAirDate: TextView by lazy { itemView.findViewById(R.id.tv_air_date) }
+
+        fun bindData(episode: Episode) {
+            tvName.text = episode.name
+            tvEpisode.text = episode.episode
+            tvAirDate.text = episode.airDate
+        }
+    }
+
+
 }
--- app/src/main/java/da/zo/rickandmortyapi/episodes/presentation/view/EpisodesFragment.kt
+++ app/src/main/java/da/zo/rickandmortyapi/episodes/presentation/view/EpisodesFragment.kt
@@ -5,56 +5,75 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import da.zo.rickandmortyapi.R
+import da.zo.rickandmortyapi.databinding.FragmentEpisodesBinding
+import da.zo.rickandmortyapi.episodes.domain.model.Episode
+import da.zo.rickandmortyapi.episodes.domain.model.Episodes
+import da.zo.rickandmortyapi.episodes.presentation.viewmodel.EpisodesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EpisodesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class EpisodesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    @Inject
+    lateinit var episodeViewModel: EpisodesViewModel
+    private var episodesBinding: FragmentEpisodesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_episodes, container, false)
+    ): View = FragmentEpisodesBinding.inflate(inflater, container, false).also {
+        episodesBinding = it
+        initViews()
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        episodesBinding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                episodeViewModel.episodes.collect { ep ->
+                    if (ep != null) {
+                        loadEpisodes(data = ep)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadEpisodes(data: Episodes) {
+        (episodesBinding?.rvEpisodesData?.adapter as? EpisodeAdapter)?.updateData(newData = data.results)
+    }
+
+    private fun initViews() {
+        with(episodesBinding?.rvEpisodesData) {
+            this?.layoutManager = LinearLayoutManager(context)
+            this?.adapter = EpisodeAdapter()
+            this?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val layoutManager: LinearLayoutManager? =
+                        recyclerView.layoutManager as LinearLayoutManager?
+                    if (layoutManager?.findLastCompletelyVisibleItemPosition() == recyclerView.adapter?.itemCount?.minus(1)) {
+                        episodeViewModel.onEndOfScrollReached()
+                    }
+                }
+            })
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EpisodesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EpisodesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(): EpisodesFragment = EpisodesFragment().apply { arguments = Bundle() }
     }
 }
