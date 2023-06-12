3d677a0601ba2ca751ac575d59ebd9459e9a363d
David Muñoz <david.munyoz97@gmail.com>
2023-06-12 19:41:04 +0200

    fix: Added and modified usecases for filters to upgrade app architecture

--- app/src/main/java/da/zo/rickandmortyapi/characters/domain/usecase/GetCharactersByGenderUc.kt
+++ app/src/main/java/da/zo/rickandmortyapi/characters/domain/usecase/GetCharactersByGenderUc.kt
@@ -1,19 +1,16 @@
 package da.zo.rickandmortyapi.characters.domain.usecase
 
-
 import da.zo.rickandmortyapi.characters.domain.CharacterDomainLayerContract
 import da.zo.rickandmortyapi.characters.domain.model.Characters
-import da.zo.rickandmortyapi.characters.presentation.view.CharacterFragment
-import da.zo.rickandmortyapi.databinding.FragmentCharacterBinding
 import javax.inject.Inject
 
 //
-// Created by DaZo20 on 25/02/2023.
+// Created by DaZo20 on 03/06/2023.
 // Copyright (c) 2023 DZ. All rights reserved.
 //
-class GetCharactersByNameUC @Inject constructor(
+class GetCharactersByGenderUc @Inject constructor(
     private val characterRepository: CharacterDomainLayerContract.DataLayer.CharacterRepository
 ) {
-    suspend operator fun invoke(name: String): Result<Characters> =
-        characterRepository.getCharactersByName(name)
+    suspend operator fun invoke(gender: String): Result<Characters> =
+        characterRepository.getCharactersByGender(gender)
 }
